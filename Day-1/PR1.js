ed9eb910ee138b55710c7a08702500c62d9f77bd
fadj-Reza88 <fadjrainreza@gmail.com>
2019-07-31 15:55:10 +0700

    Terserah

---
+++ Day-1/PR1.js
@@ -0,0 +1,7 @@
+var sandwich = 8
+var salad = 7
+var burger = 10
+var kentang = 5
+var totalHarga = (4*sandwich) + (5*salad) + (2*burger) + (7*kentang)
+    console.log('$' + totalHarga)
+ 
---
+++ Day-1/index.js
@@ -0,0 +1,24 @@
+// var bebas = 'ini string'    // tipe data string
+// var terserah = '1'           // tipe data Number
+// var gatau = '3'
+
+<script>
+    
+    var galau = 1
+    var php = 2
+    var sakithati = 3 
+    var ditikung = '4'
+
+console.log(galau)
+</script>
+// var aku = 'dia' // var bisa diganti dan dideclare lagi
+var aku = 'mereke'
+
+// let itGO // let bisa diganti tapi gabisa dideclare
+
+// const gravitasi = 9.8 // const sama sekali tidak bisa diganti dan dideclare lagi
+
+var benar = true
+var salah = false
+
+console.log(typeof salah)
